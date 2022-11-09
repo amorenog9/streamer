b89bddd1aba25b206cade446691a5f0f648a3975
amorenog9 <alejandro.moreno.garcia@alumnos.upm.es>
2022-11-09 17:43:59 +0100

    Initial commit - without /data

---
+++ sample_streamer.py
@@ -0,0 +1,25 @@
+from time import sleep
+import pandas as pd
+import socket
+
+
+def generator(data):
+    deltas = data.delta.values
+    data_rows = data.drop(columns=["delta"])
+    for i in range(len(data)):
+        sleep(deltas[i])
+        yield data_rows.iloc[i].to_json()+"\n"
+
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind(("localhost",9090))
+print("hello1")
+socket.listen()
+while True:
+    conn, addr = socket.accept()
+    print("hello")
+    with conn:
+        print(f"Connected by {addr}")
+        g = generator(pd.read_feather("data/anubis.feather"))
+        for line in g:
+            conn.send(line.encode())
