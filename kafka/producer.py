79197bd77237758f33cc2834fdcd88db3861981e
amorenog9 <alejandro.moreno.garcia@alumnos.upm.es>
2022-11-29 11:34:57 +0100

    Json Date Understanding

--- kafka/producer.py
+++ kafka/producer.py
@@ -14,9 +14,9 @@ def generator(data):
    # deltas = data.delta.values
    # data_rows = data.drop(columns=["delta"])
     for i in range(len(data)):
-        sleep(0.2)
-        #sleep(deltas[i])
-        yield data.iloc[i].to_json()+"\n"
+        sleep(0.2) #sleep(deltas[i])
+        yield data.iloc[i].to_json()+"\n" #Por defecto on date_format = 'epoch' -> 1484856900000 (epoch milliseconds)
+        #yield data.iloc[i].to_json(date_format='iso')+"\n" #Con date_format = 'iso' -> "2017-01-19T20:15:00.000"
         #yield data_rows.iloc[i].to_json()+"\n"
 
 if __name__ == '__main__':
