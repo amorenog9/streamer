45b4308a1cdea67d2bde945e663403f00429d1f1
amorenog9 <alejandro.moreno.garcia@alumnos.upm.es>
2022-12-16 18:52:34 +0100

    Docker v3

--- kafka/KafkaDocker3/kafka2/start.sh
+++ kafka/KafkaDocker3/kafka2/start.sh
@@ -13,14 +13,7 @@
   --replication-factor 1 \
   --partitions 1 \
   --topic messages_out &
-  
-./bin/kafka-topics.sh \
-  --create \
-  --bootstrap-server localhost:9092 \
-  --replication-factor 1 \
-  --partitions 1 \
-  --topic temporal_messages
-  
+
   
 
 
