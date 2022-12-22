5c213a612eee5aea5ff40bd3294294687ae35014
amorenog9 <alejandro.moreno.garcia@alumnos.upm.es>
2022-12-22 12:33:47 +0100

    Delete unnecesary topics

--- kafka/KafkaDocker/kafka2/start.sh
+++ kafka/KafkaDocker/kafka2/start.sh
@@ -19,14 +19,7 @@
   --bootstrap-server localhost:9092 \
   --replication-factor 1 \
   --partitions 1 \
-  --topic messages_from_timestamp_in &
-
-./bin/kafka-topics.sh \
-  --create \
-  --bootstrap-server localhost:9092 \
-  --replication-factor 1 \
-  --partitions 1 \
-  --topic messages_from_timestamp_out
+  --topic messages_from_timestamp
 
 
 
