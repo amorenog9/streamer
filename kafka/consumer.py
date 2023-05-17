c775158c0c5c99ac0c838a54222d407ae9fba7ee
amorenog9 <alexmgtrb@gmail.com>
2023-05-17 23:07:12 +0200

    creacion de subred docker

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # Kafka Consumer
     consumer = KafkaConsumer(
         KAFKA_TOPIC,
-        bootstrap_servers='localhost:9092',
+        bootstrap_servers='kafka:9092',
         auto_offset_reset='earliest',
         #auto_offset_reset='latest',
         api_version=(3, 0, 0)
